aa72a3bedd19342c64bb5cf57eeadf9ca11a26b6
Kalogon <ghlee5084@kaist.ac.kr>
2024-10-28 16:17:41 +0900

    Add IC8

---
+++ queries/IC8.sql
@@ -0,0 +1,21 @@
+/* Q8. Recent replies
+\set personId 933
+ */
+SELECT personId, personFirstName, personLastName, commentCreationDate, 
+        commentId, commentContent
+FROM cypher($$
+MATCH (start:person)<-[:message_hascreator_person]-(:message)<-[:message_replyof_message]-(comment:message)-[:message_hascreator_person]->(dest:person)
+WHERE start.vertex_id = 2199023819768
+RETURN
+    dest.vertex_id AS personId,
+    dest.firstname AS personFirstName,
+    dest.lastname AS personLastName,
+    comment.creationdate AS commentCreationDate,
+    comment.vertex_id AS commentId,
+    comment.content AS commentContent
+ORDER BY
+    comment.creationdate DESC,
+    comment.vertex_id ASC
+LIMIT 20
+$$) as (personId bigint, personFirstName varchar, personLastName varchar, commentCreationDate timestamp, 
+        commentId bigint, commentContent text);
